5106dd73cd4e1eda11269657c175dae5d0e0f994
Jack Willis <jack@attac.us>
2023-03-31 17:29:33 -0500

    Add indexes

--- database/schema.sql
+++ database/schema.sql
@@ -15,8 +15,11 @@ CREATE TABLE IF NOT EXISTS `warns` (
 CREATE TABLE IF NOT EXISTS `activity` (
   `id` int(11) NOT NULL
     CONSTRAINT activity_pk PRIMARY KEY AUTOINCREMENT,
-  `user_id` varchar(20) NOT NULL,
-  `type` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL
+    INDEX activity_user_id_idx,
+  `type` varchar(20) NOT NULL
+    INDEX activity_type_idx,
   `created_at` timestamp NOT NULL
+    INDEX activity_created_at_idx
     DEFAULT CURRENT_TIMESTAMP
 );
