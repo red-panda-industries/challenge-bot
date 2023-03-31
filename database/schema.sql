221271cda70c55bce7d525d2057d5d2b444fd768
Jack Willis <jack@attac.us>
2023-03-31 17:34:36 -0500

    Fix names

--- database/schema.sql
+++ database/schema.sql
@@ -15,10 +15,10 @@ CREATE TABLE IF NOT EXISTS `warns` (
 CREATE TABLE IF NOT EXISTS `activity` (
   `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `user_id` varchar(20) NOT NULL,
-  `type` varchar(20) NOT NULl,
+  `activity_type` varchar(20) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX activity_user_id_index ON activity(user_id);
-CREATE INDEX activity_type_index ON activity(type);
+CREATE INDEX activity_type_index ON activity(activity_type);
 CREATE INDEX activity_created_at_index ON activity(created_at);
