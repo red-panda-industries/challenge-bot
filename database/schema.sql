207be6669d13745c4dbed47388b7d381f69c34b3
Jack Willis <jack@attac.us>
2023-03-31 17:28:23 -0500

    Add activity table

--- database/schema.sql
+++ database/schema.sql
@@ -11,3 +11,12 @@ CREATE TABLE IF NOT EXISTS `warns` (
   `reason` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS `activity` (
+  `id` int(11) NOT NULL
+    CONSTRAINT activity_pk PRIMARY KEY AUTOINCREMENT,
+  `user_id` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `created_at` timestamp NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+);
